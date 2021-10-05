bce509393d9a66d27749a68189a88e9d8b65c981
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-05 21:42:22 +1100

    Pre starter files

--- menu-one/src/App.js
+++ menu-one/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Categories from './Components/Categories'
+import Menu from './Components/Menu'
 import "./CSS/index.css"
+import data from "./Components/data.js"
 
 const App = () => {
+    const [menu, setMenu] = useState(data);
+    const [categories, setCategories] = useState([]);
     return (
-        <div>
-            <h1> test</h1>
-        </div>
+        <main> 
+         <section className='menu section'>
+             <div className='title'>
+                 <h2>KK's Kitchen</h2>
+                 <div className="underline">
+
+                 </div>
+<Categories/>
+<Menu  menuItems = {menu}/>
+             </div>
+
+         </section>
+        </main>
     )
 }
 
---
+++ menu-one/src/Components/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            <h1> test</h1>
+            
+        </div>
+    )
+}
+
+export default Categories
