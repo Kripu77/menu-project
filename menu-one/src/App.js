03349c049f2d5adda3dff0544e16f4fee9c5a1b0
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-06 12:06:27 +1100

    Completed

--- menu-one/src/App.js
+++ menu-one/src/App.js
@@ -6,24 +6,27 @@ import "./CSS/index.css"
 
 const allBtns = new Set(data.map((value)=> value.category)) //set method only returns unique value i.e. not repeated but in terms of object 
 
-
+//converting the object into array
 const finalBtn = Array.from(allBtns);
-finalBtn.push('all');
-console.log(finalBtn);
+finalBtn.push('all'); //pushing all option to the menu
+
+console.log(finalBtn); //check
 
 
 const App = () => {
+    document.title="KK's Kitchen Menu"
     const [menuItems, setMenuItems] = useState(data);
-
+// the fn will filter as per the function is invoked in the menu component
     const filterFn = (value)=>{
 
         if(value==="all"){
             return setMenuItems(data);
         }
-         setMenuItems(data.filter((values)=>{
+        // when the value is obtained as certain specfic category then it will only return that particular category
+       else{  setMenuItems(data.filter((values)=>{
              return values.category === value;
          }))
-    }
+    }}
 
     return (
        <main>
